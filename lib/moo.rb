176b4dae17df68d946dd27b7d7f9cf542b3c4c64
James Darling <abscond@Sam.local>
2008-08-25 18:56:10 +0100

    set a string target for xml. Fixes <to_s/> problem

--- lib/moo.rb
+++ lib/moo.rb
@@ -2,6 +2,8 @@ require 'core_ext/options_struct'
 require 'rubygems'
 require 'builder'
 require 'design'
+require 'net/http'
+require 'uri'
 
 module Moo
   class Order < OptionsStruct.create(:api_key, :designs)
@@ -20,14 +22,16 @@ module Moo
       @designs.first.product_type if @designs.first
     end
     
-    def to_xml(xml=Builder::XmlMarkup.new)
+    def to_xml(xml=nil)
+      xml = Builder::XmlMarkup.new(:target => (@xml_s ||= "")) unless xml
       eval File.open(File.expand_path(File.dirname(__FILE__) + "/template.xml.builder")).read
-      xml
+      xml.target!
     end
     
     def submit
-      @api = API.new
-      @api.send_order(self)
+      res = Net::HTTP.post_form(URI.parse('http://www.moo.com/api/api.php'),
+                                    {'xml'=>self.to_xml, 'method'=>'direct'})
+      puts res.body
     end
   end
   
